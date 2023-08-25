7cc97a9e1a57a8a76709eb93ab0064bc6beed461
RaisanJMR <raisanjmr@protonmail.com>
2023-08-25 16:34:47 +0530

    DEPLOY: prepared for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorHandler')
@@ -13,14 +14,22 @@ app.options('*', cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
-const PORT = 8000
+const PORT = process.env.PORT || 5000
 app.use(errorHandler)
 
 app.use('/api', require('./routes/userRoutes'))
 
-app.get('/', (req, res) => {
-  res.send('api is running...')
-})
+const __dirname1 = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, '/client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('api is running...')
+  })
+}
 
 app.listen(PORT, () =>
   console.log(
